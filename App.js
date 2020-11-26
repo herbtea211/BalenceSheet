c3b9642914e199cb974cc5493d45193979511ef6
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2020-11-26 12:13:25 +0800

    add bottomTab done

--- App.js
+++ App.js
@@ -6,33 +6,81 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   StyleSheet,
-  SafeAreaView,
-  Text,
   StatusBar,
+  View,
+  Text,
+  SafeAreaView
 } from 'react-native';
 
 import {} from 'react-native/Libraries/NewAppScreen';
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <FontAwesomeIcon icon={ faCoffee } />
-        <Text>I am hugo</Text>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends React.Component {
 
-const styles = StyleSheet.create({
+  constructor(props) {
+    super(props)
 
-});
+    this.state = {
+      BottomTab: null
+    }
 
-export default App;
+  } // constructor end
+
+  createBottomTabNavigator() {
+   
+    const bottomTab = createBottomTabNavigator()
+
+    return bottomTab
+  }
+
+  indexScreen() {
+    return (
+      <Text>home</Text>
+    )
+  }
+
+  setScreen() {
+    return (
+      <Text>Account</Text>
+    )
+  }
+
+  topSearch() {
+    return (
+      <Text>topSearch</Text>
+    )
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+
+    const BottomTab = this.createBottomTabNavigator()
+    
+    return (
+      <>
+        <StatusBar/>
+        <SafeAreaView style={{flex: 1,backgroundColor: '#eaeaea'}}>
+          <View>
+            <Text>topSearch</Text>
+          </View>
+          <NavigationContainer>
+              <BottomTab.Navigator>
+                <BottomTab.Screen name='首頁' component={ this.indexScreen }></BottomTab.Screen>
+                <BottomTab.Screen name='設定' component={ this.setScreen }></BottomTab.Screen>
+              </BottomTab.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </>
+      
+    )
+  }
+} // class App end
