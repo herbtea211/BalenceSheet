4732dda0189cc7a2c1bcc3ba31145174a0f386a5
herbtea211 <33927101+herbtea211@users.noreply.github.com>
2020-12-04 11:26:15 +0800

    search bar

--- components/TopSearchComponent.js
+++ components/TopSearchComponent.js
@@ -3,9 +3,70 @@ import React,{Component} from 'react';
 import {
     StyleSheet,
     Text,
-    TouchableHighlight,
-    View
+    View,
+    TouchableOpacity,
+    TextInput
 } from 'react-native';
+
+export class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+
+        // const [value, onChangeText] = React.useState('Useless Placeholder');
+        return (
+            <>
+                <TextInput
+                style={styles.searchBarStyle}
+                // onChangeText={() => {
+
+                // }}
+                value={'我想找...'}
+                ></TextInput>
+            </>
+        )
+    }
+}
+
+export class MenuBtnComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    _createLineComponent() {
+
+        const lineArray = []
+
+        for(let i = 0;i < 2;i++) {
+
+            const _marginTop = (i !== 0) ? 7 : 0
+
+            let _component = <View style={[styles.lineStyle, { marginTop: _marginTop }]} key={i}></View>
+
+            lineArray.push(_component)
+
+        }
+
+        return lineArray
+    }
+
+    render() {
+        return (
+            <>
+                {this._createLineComponent()}
+            </>
+            
+        )
+    }
+}
 
 
 
@@ -21,24 +82,49 @@ export default class TopSearchComponent extends Component {
         return (
             <View
                 style={styles.topSearchBarStyle}>
-                <TouchableHighlight
+                <TouchableOpacity
                     onPress={() => {
                         this.props.setStateShowMenu()
                     }}>
-                        <Text>openMenu</Text>
-                    </TouchableHighlight>
+                        <MenuBtnComponent/>
+                    </TouchableOpacity>
+                <SearchBar/>
+                <Text>bar</Text>
             </View>
         )
         
     }
 }
 
+const paddingHor = 10
+const topSearchBarStyle = 70
+const searchBarMarginHor = 10
+
 const styles = StyleSheet.create({
     topSearchBarStyle: {
-        backgroundColor: '#eaeaea',
-        height: 70,
+        height: topSearchBarStyle,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingLeft: paddingHor,
+        paddingRight: paddingHor
+    },
+    lineStyle: {
+        width: 30,
+        height: 2,
+        backgroundColor: '#000',
+        borderRadius: 100
+    },
+    searchBarStyle: {
+        height: topSearchBarStyle - 20,
+        borderColor: '#f6f6f6',
+        borderWidth: 1,
+        flex: 1,
+        marginLeft: searchBarMarginHor,
+        marginRight: searchBarMarginHor,
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#f6f6f6'
     }
 })
 
